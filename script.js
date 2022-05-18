d86d1488564c8fa67a8dc2e5769e49a8f2de9d16
Glitch (emerald-cute-physician) <none>
2022-05-18 21:30:22 +0000

    📪➗ Updated with Glitch

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 //movie api url
-//https://api.themoviedb.org/3/search/movie?api_key=1de8557f26f4d177fcb5b21811677161&language=en-US&query=findingdory
+let movieapi =
+  "https://api.themoviedb.org/3/discover/movie?api_key=73fd371dd4dae96ea8f80a479504448a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+//17 items in array
+//to get genre list we have to do another api call
+//ratings is out of 10
 
-//movie poster url
-//https://image.tmdb.org/t/p/w500${moviePosterData}
+fetch(movieapi)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(movieapi);
+    for (let i = 0; i <= data.results.length; i++) {
+      let posterPath = data.results[i].poster_path;
+      console.log(posterPath);
+      $(".movieTitle").append(`<h1>${data.results[i].title}</h1>`);
+      $(".releaseDate").append(`<p>${data.results[i].release_date}</p>`);
+      $(".moviePoster").append("<img src = https://image.tmdb.org/t/p/original/" + posterPath+">");
+      $(".bio").append(`<p>${data.results[i].overview}</p>`);
+      $(".rating").append(`<p>${data.results[i].vote_average}</p>`);
 
+    }
 
-//declare global variables
-
-//include a click handler for search bar
-
-//save user input for city
-//save user input for state
-
-//update movie api
-
-//add fetch request using movie api
-
-//then with function that returns an api response in json
-
-//then with function that displays data to page
-//loop over data to show multiple movies
+   
+    
+  });
